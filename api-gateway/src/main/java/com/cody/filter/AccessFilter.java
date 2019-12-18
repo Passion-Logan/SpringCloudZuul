aab976861eaa3969278a046e6aea86ec83b7525d
passion-logan <wangdadamailbox@163.com>
2019-12-18 18:52:12 +0800

    修改过滤器

--- api-gateway/src/main/java/com/cody/filter/AccessFilter.java
+++ api-gateway/src/main/java/com/cody/filter/AccessFilter.java
@@ -1,5 +1,9 @@
 package com.cody.filter;
 
+import com.netflix.zuul.ZuulFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 应用模块名称<p>
  * 代码描述<p>
@@ -8,5 +12,27 @@ package com.cody.filter;
  * @author WQL
  * @since 2019年12月18日 0018 18:50
  */
-public class AccessFilter {
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() {
+        return null;
+    }
 }
