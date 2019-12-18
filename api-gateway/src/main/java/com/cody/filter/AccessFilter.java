a170ce4ed218420322ec58013a46a83f03dd23fc
passion-logan <wangdadamailbox@163.com>
2019-12-18 18:50:44 +0800

    添加过滤器

---
+++ api-gateway/src/main/java/com/cody/filter/AccessFilter.java
@@ -0,0 +1,12 @@
+package com.cody.filter;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年12月18日 0018 18:50
+ */
+public class AccessFilter {
+}
