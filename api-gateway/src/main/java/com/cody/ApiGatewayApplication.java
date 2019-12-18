68a693cb5b88d13cbb3ffb85091d03f40b2162e6
passion-logan <wangdadamailbox@163.com>
2019-12-18 18:58:20 +0800

    添加注释

--- api-gateway/src/main/java/com/cody/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/cody/ApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.cody;
 
+import com.cody.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 使用@EnableZuulProxy注解开启Zuul的功能
@@ -13,6 +15,11 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 
 }
